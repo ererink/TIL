772aa8f9ef0961c862a5e9f6d28f076a0da951a0
ererink <yelkim0210@gmail.com>
2022-07-14 18:01:30 +0900

    Add 1

---
+++ codeup/6052.py
@@ -0,0 +1,7 @@
+# 정수가 입력되었을 때, True/False 로 평가해주는 프로그램을 작성해보자.
+
+n = int(input())
+
+print(bool(n)) #boolean을 사용해 True/False로 평가해주었다. 
+
+# bool()은 정수값 0은 False(거짓)로 평가되고, 그 외의 값들은 모두 True(참)로 평가된다.
