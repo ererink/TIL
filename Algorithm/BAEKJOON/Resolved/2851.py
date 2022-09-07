4322f52adcfd91edefc9e7b7463a888e1c7469b6
ererink <yelkim0210@gmail.com>
2022-09-08 00:48:21 +0900

    Add solutions

---
+++ Algorithm/BAEKJOON/Resolved/2851.py
@@ -0,0 +1,22 @@
+score = 0
+
+for _ in range(10):
+    copy_score = score
+    score += int(input())
+    if score >= 100:
+        break
+
+
+    
+if score > 100:
+    under_ = 100 - copy_score
+    over_ = score - 100
+    if under_ > over_:
+        print(score)
+    elif under_ < over_:
+        print(copy_score)
+    else:
+        print()
+    
+else: 
+    print(score)
---
+++ Algorithm/BAEKJOON/Resolved/2941.py
@@ -0,0 +1,10 @@
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia:
+    if i in word:
+        word = word.replace(i, '!')
+    else:
+        continue
+
+print(len(word))
