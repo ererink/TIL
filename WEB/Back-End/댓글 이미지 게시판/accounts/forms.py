017be676823f4437359c7593d26ebea8235c7664
ererink <yelkim0210@gmail.com>
2022-10-18 21:27:41 +0900

    Add Comment func

---
+++ WEB/Back-End/댓글 이미지 게시판/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm 
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username','email',)
+    
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email',)
---
+++ WEB/Back-End/댓글 이미지 게시판/articles/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Article
+from .forms import ArticleForm, CommentForm
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('pk')
+
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    
+    else: 
+        article_form = ArticleForm()
+    context = {
+        'article_form': article_form
+    }
+    
+    return render(request, 'articles/form.html', context=context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    
+    comment_form = CommentForm()
+   
+    context = {
+        'article': article,
+        'comments': article.comment_set.all(),
+        'comment_form': comment_form,
+    }
+    return render(request, 'articles/detail.html', context)
+
+@login_required
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+
+    else:
+
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/form.html', context)
+
+def comment_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk)
