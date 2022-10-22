445799627875dcdaf936bc787611a6b8dba38097
ererink <yelkim0210@gmail.com>
2022-10-22 23:52:20 +0900

    Add solutions

---
+++ Algorithm/Programmers/부족한 금액 계산하기.py
@@ -0,0 +1,13 @@
+def solution(price, money, count):
+    answer = 0
+    check = 0
+
+    for i in range(1, count + 1):
+        check += price * i 
+    
+    if check - money > 0:
+        answer = check - money
+    else:
+        answer = 0
+    
+    return answer
