da541dae9db656004d18401643e3824cf014008e
ererink <yelkim0210@gmail.com>
2022-10-26 16:47:32 +0900

    Add practice

---
+++ WEB/Back-End/20 좋아요:댓글 비동기 처리/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('update/', views.update, name='update'),
+    path('<int:pk>/follow/', views.follow, name='follow'),
+]
