1849b1dcf1236657dc5894df9c8276bd4203c254
ererink <yelkim0210@gmail.com>
2022-11-03 21:58:44 +0900

    Add a solution 11.03.22

---
+++ Algorithm/Programmers/SQL/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,8 @@
+-- PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요. 
+-- 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
+
+SELECT PRODUCT_CODE, SUM(P.PRICE * O.SALES_AMOUNT) AS SALES
+FROM PRODUCT P INNER JOIN OFFLINE_SALE O
+ON P.PRODUCT_ID = O.PRODUCT_ID
+GROUP BY P.PRODUCT_CODE
+ORDER BY SALES DESC, P.PRODUCT_CODE
