f5773547f88d8250e4213b600290a5551d05f210
ererink <yelkim0210@gmail.com>
2022-11-25 23:57:41 +0900

    add

--- Algorithm/BAEKJOON/1427.py
+++ Algorithm/BAEKJOON/1427.py
@@ -9,3 +9,16 @@ N = sorted(N, reverse=True)
 result = "".join(N)
 print(result)
 
+a = input()
+
+lis=[]
+lis2=[]
+
+lis.append(a)
+for i in lis:
+    for j in i:
+        lis2.append(j)
+
+lis2.sort(reverse=True)
+b = "".join(lis2) 
+print(b)
