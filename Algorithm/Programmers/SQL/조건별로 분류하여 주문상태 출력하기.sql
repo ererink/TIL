45ab5d593f41be02efef23ca197b3bc357f4f702
ererink <yelkim0210@gmail.com>
2023-01-05 00:09:49 +0900

    01.05.23

---
+++ Algorithm/Programmers/SQL/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,8 @@
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d'),
+    CASE
+        WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+        WHEN OUT_DATE > '2022-05-01' THEN '출고대기'
+        WHEN OUT_DATE IS NULL THEN '출고미정'
+        END AS '출고여부'
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
