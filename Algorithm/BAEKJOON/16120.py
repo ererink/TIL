3f39d54d06e6cdaed4b963b3b8c227754ecebe95
ererink <yelkim0210@gmail.com>
2023-01-12 17:46:56 +0900

    01.12.23

--- Algorithm/BAEKJOON/16120.py
+++ Algorithm/BAEKJOON/16120.py
@@ -7,6 +7,7 @@ for i in word:
     stack.append(i)
     
     # stack에 할당된 값이 PPAP일 때 맨 앞의 P를 남겨두고 나머지 3개의 문자를 빼준다.
+    # 일치한다면 PPAP를 P로 변환하는 작업
     if stack[-4:] == ppap:
         stack.pop()
         stack.pop()
