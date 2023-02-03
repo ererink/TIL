5a754f04e91bc22cb58b2316c1c39f050830b384
ererink <yelkim0210@gmail.com>
2023-02-03 16:28:43 +0900

    02.03.23

---
+++ Algorithm/BAEKJOON/1753.py
@@ -0,0 +1,42 @@
+from heapq import heappop, heappush
+import sys
+
+INF = sys.maxsize
+
+n, m = map(int, input().split())
+start = int(input())
+start -= 1
+
+maps = [[] for _ in range(n)]
+visited = [INF] * n
+visited[start] = 0
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    a -= 1
+    b -= 1
+    
+    maps[a].append((cost, b))
+
+heap = []
+heappush(heap, (0, start))
+
+while heap:
+    cost, x = heappop(heap)
+    
+    # 이미 적은 비용으로 지나간 최솟값이 있다면 밑 구문을 실행할 수 없다
+    if visited[x] < cost:
+        continue
+    
+    for k, v in maps[x]:
+        min_cost = cost + k
+        
+        if visited[v] > min_cost:
+            visited[v] = min_cost
+            heappush(heap, (min_cost, v))
+
+for i in visited:
+    if i == INF:
+        print('INF')
+    else:
+        print(i)
