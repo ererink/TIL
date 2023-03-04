bfdfe4c1646718362bbb0595f46dac4e51a7e4cc
ererink <yelkim0210@gmail.com>
2023-03-04 23:08:48 +0900

    03.04.23

--- Algorithm/BAEKJOON/10807.java
+++ Algorithm/BAEKJOON/10807.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] array = new int[N];
-        int cnt = 0;
+        int N = sc.nextInt();                   // 정수의 개수
+        int[] array = new int[N];               // 배열 만들기
+        int cnt = 0;                            // 특정한 정수 카운트 변수
 
         for(int i = 0; i < N; i++) {
-            array[i] = sc.nextInt();
+            array[i] = sc.nextInt();            // 배열 입력값 받기
         }
 
-        int v= sc.nextInt();
+        int v= sc.nextInt();                    // 특정한 정수 입력값 받기
 
         for(int i = 0; i < array.length; i++) {
-            if (v == array[i]){
-                cnt++;
+            if (v == array[i]){                 // 특정한 정수 찾기
+                cnt++;                          // cnt + 1
             }
         }
 
