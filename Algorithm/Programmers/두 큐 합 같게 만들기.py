6988115787d899c5afa921e7b59b5fb8351d94e4
ererink <yelkim0210@gmail.com>
2023-04-27 22:04:02 +0900

    04.27.23

--- Algorithm/Programmers/두 큐 합 같게 만들기.py
+++ Algorithm/Programmers/두 큐 합 같게 만들기.py
@@ -3,14 +3,13 @@ def solution(queue1, queue2):
     answer = 0
     n = len(queue1)
     queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    
+    queue2 = deque(queue2)    
     q1 = 0; q2 = 0
-
+    
     for i in range(n):
         q1 += queue1[i]
         q2 += queue2[i]
-
+    
     while q1 != q2:
         if q1 > q2:
             temp = queue1.popleft()
@@ -29,4 +28,9 @@ def solution(queue1, queue2):
         if q1 == 0 or q2 == 0:
             answer = -1
             break
+        
+        if answer > 2*n + 2:
+            answer = -1
+            break
+
     return answer
