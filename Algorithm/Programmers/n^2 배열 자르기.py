4180a1b4820f90c8a17dcde56bd7b540bc36f255
ererink <yelkim0210@gmail.com>
2023-08-14 20:17:15 +0900

    08.14.23

--- Algorithm/Programmers/n^2 배열 자르기.py
+++ Algorithm/Programmers/n^2 배열 자르기.py
@@ -1,3 +1,10 @@
+def solution(n, left, right):
+    answer = []
+
+    for i in range(left, right + 1):
+        answer.append(max(i//n, i%n) + 1)
+    return answer
+
 # 1차 시도
 def solution(n, left, right):
     new_arr = []
