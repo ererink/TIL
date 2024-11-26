db1bb2a157d1157f265ac7e3afcf4ccf78ee164c
ererink <yelkim0210@gmail.com>
2024-11-26 20:36:55 +0900

    삼각형 컨테이너 벨트

---
+++ Algorithm/CodeTree/삼각형 컨테이너 벨트.py
@@ -0,0 +1,20 @@
+# 시뮬레이션
+from collections import deque
+
+n, t = map(int, input().split())
+queue = deque([])
+for i in range(3):
+    temp = list(map(int, input().split()))
+    for j in range(n):
+        queue.append(temp[j])
+
+for i in range(t):
+    temp = queue.pop()
+    queue.appendleft(temp)
+
+idx = 0
+for i in range(n*3):
+    print(queue[idx], end=" ")
+    idx += 1
+    if idx % n == 0:
+        print()
